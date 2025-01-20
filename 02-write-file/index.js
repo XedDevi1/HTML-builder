b0add502b0dcf484ca822c5c70e2a0667a015cf6
XedDevi1 <vadim.presman@gmail.com>
2025-01-20 18:26:39 +0100

    feat: add realization for 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+if (!fs.existsSync(filePath)) {
+  fs.closeSync(fs.openSync(filePath, 'w'));
+}
+
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Hello! Input text here (enter "exit" to quit):');
+
+rl.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    rl.close();
+  } else {
+    writeStream.write(`${input}\n`, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err.message);
+      }
+    });
+  }
+});
+
+const exitHandler = () => {
+  console.log('Have a good day! :)');
+  rl.close();
+  writeStream.end();
+  process.exit();
+};
+
+rl.on('close', exitHandler);
+
+process.on('SIGINT', exitHandler);
